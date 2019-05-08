92b118face799ebbc3e0b4ba305d7f5647eb8dab
Baraa <baraadarwish5@gmail.com>
2019-05-08 14:10:07 +0300

    'styling'

--- CABBH/settings.py
+++ CABBH/settings.py
@@ -153,8 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-    
+STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, 'static/')  # add STATIC_ROOT to DIRS
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 MEDIA_URL = '/media/' 
